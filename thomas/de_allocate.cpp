58ffb75db8f4e934ac54a70fb8a610d4598f9a09
vvickia <victoria.kobozeva@gmail.com>
2023-12-16 16:01:40 +0300

    add task of thomas algh

---
+++ thomas/de_allocate.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+
+// Functions for allocating/deallocating memory
+double ** create_array (size_t a, size_t b)
+{
+    double ** m = new double *[a];
+    m[0] = new double[a * b];
+    for (size_t i = 1; i != a; ++i)
+        m[i] = m[i - 1] + b;
+    return m;
+}
+
+void free_array (double ** m)
+{
+    delete [] m[0];
+    delete [] m;
+}
